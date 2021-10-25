7b7e9ba393e27c3a4481f6656af11006cd2c4038
nikolay-evsevitskiy <nikolay.evsevitskiy@gmail.com>
2021-10-25 18:54:13 +0300

    used Profile State data

--- src/componets/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/componets/Profile/ProfileInfo/ProfileInfo.tsx
@@ -8,7 +8,7 @@ type ProfileInfoType = {
 
 
 const ProfileInfo = (props: ProfileInfoType) => {
-    if (!props.profile.photos) {
+    if (!props.profile.photos || !props.profile) {
         return <Preloader isFetching={true}/>
     }
     return (
@@ -18,8 +18,32 @@ const ProfileInfo = (props: ProfileInfoType) => {
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjYFV-bwRLTx5vbXeIRyRZDH86KNG-4ktGcg&usqp=CAU"/>
             </div>
             <div>
+                <div>Ava + description</div>
                 <img src={props.profile.photos.large}/>
-                Ava + description
+                <div>Full name: {props.profile.fullName}</div>
+                <div>
+                    About me: {props.profile.aboutMe}
+                </div>
+                <div>
+                    Contacts:
+                    <ul>
+                        <li>facebook: {props.profile.contacts.facebook}</li>
+                        <li>website: {props.profile.contacts.website}</li>
+                        <li>vk: {props.profile.contacts.vk}</li>
+                        <li>twitter: {props.profile.contacts.twitter}</li>
+                        <li>instagram: {props.profile.contacts.instagram}</li>
+                        <li>youtube: {props.profile.contacts.youtube}</li>
+                        <li>github: {props.profile.contacts.github}</li>
+                        <li>mainLink: {props.profile.contacts.mainLink}</li>
+                    </ul>
+                </div>
+                <div>
+                    <p>
+                        LOOKING FOR A JOB!!!!
+                    </p>
+                    <p>Description: {props.profile.lookingForAJobDescription}</p>
+                </div>
+
             </div>
 
         </div>
